60f76e5a03ea3394a8d0d5f70f55096c49da8a52
Ross Wilson <me@rosswilson.co.uk>
2016-07-14 17:31:20 +0100

    Adding browser link to server start output

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ app.use(express.static('public'));
 
 app.listen(3030, function () {
   console.log('Studio Clock app listening on port 3030');
+  console.log('Visit http://localhost:3030 in a browser');
 });
